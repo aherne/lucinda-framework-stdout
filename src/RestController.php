48ba7e487d031c8446378c3b9a39c231947de76a
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2020-04-18 21:05:09 +0300

    moved attributes to framework

--- src/RestController.php
+++ src/RestController.php
@@ -7,37 +7,16 @@ namespace Lucinda\Framework;
 abstract class RestController extends \Lucinda\STDOUT\Controller
 {
     /**
-     * @var Attributes
-     */
-    protected $attributes;
-    
-    /**
      * {@inheritDoc}
      * @see \Lucinda\STDOUT\Runnable::run()
      */
     public function run(): void
     {
-        $this->response->view()["token"] = $this->attributes->getAccessToken();
         $methodName = strtoupper($this->request->getMethod());
         if (method_exists($this, $methodName)) {
             $this->$methodName();
         } else {
             throw new \Lucinda\STDOUT\MethodNotAllowedException();
         }
-    }
-    
-    /**
-     * Support HTTP OPTIONS requests by default
-     */
-    public function OPTIONS(): void
-    {
-        $options = array();
-        $validHTTPMethods = array("GET","POST","PUT","DELETE","HEAD","OPTIONS","CONNECT","TRACE");
-        foreach ($validHTTPMethods as $methodName) {
-            if (method_exists($this, $methodName)) {
-                $options[] = $methodName;
-            }
-        }
-        $this->response->headers("Allow", implode(", ", $options));
     }
 }
